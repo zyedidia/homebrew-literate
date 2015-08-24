4d529470114dbcc633e88472386b58eb65e7af1a
Zachary Yedidia <zyedidia@gmail.com>
2015-08-24 15:07:11 -0700

    Add tests

--- literate.rb
+++ literate.rb
@@ -11,6 +11,38 @@ class Literate < Formula
   depends_on "ctags-exuberant" => :optional
 
   def install
-      prefix.install Dir["bin/lit"]
+    bin.install Dir["bin/lit"]
+  end
+
+  test do
+    (testpath/"test.lit").write <<-EOS.undent
+      @title Hello World
+
+      @s Introduction
+      This is a simple `Hello World` program written in C.
+
+      --- hello_world.c
+      #include <stdio.h>
+
+      int main() {
+          printf("Hello world");
+
+          return 0;
+      }
+      ---
+      EOS
+
+      (testpath/"ans.c").write <<-EOS.undent
+        #include <stdio.h>
+
+        int main() {
+            printf("Hello world");
+
+            return 0;
+        }
+      EOS
+
+      system "#{bin}/lit", "-code", "#{testpath/"test.lit"}"
+      assert_match File.read("hello_world.c").strip, File.read("ans.c").strip
   end
 end
