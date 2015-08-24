52559e3f219232e7813eb259376cd73be87a2b9b
Zachary Yedidia <zyedidia@gmail.com>
2015-08-24 14:52:07 -0700

    Add --HEAD option

--- literate.rb
+++ literate.rb
@@ -4,11 +4,13 @@ class Literate < Formula
   url "http://literate.zbyedidia.webfactional.com/versions/literate.1.0.0.tar.gz"
   sha256 "c1972dd011972a993b948b93f1c2aa68c0beb1c169633f6e332e5bff0b6d833d"
 
+  head "https://github.com/zyedidia/Literate.git"
+
   depends_on "lua"
   # Better to use universal ctags
   depends_on "ctags-exuberant" => :optional
 
   def install
-      prefix.install Dir["bin"]
+      prefix.install Dir["bin/lit"]
   end
 end
