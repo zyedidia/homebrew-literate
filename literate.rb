9451fdd16df66e14520de7af22bb5496145e402e
Zachary Yedidia <zyedidia@gmail.com>
2016-01-16 15:13:54 -0500

    Update for new Literate

--- literate.rb
+++ literate.rb
@@ -1,16 +1,15 @@
 class Literate < Formula
   desc "A Modern Literate Programming System"
-  homepage "http://literate.zbyedidia.webfactional.com"
-  url "http://literate.zbyedidia.webfactional.com/versions/literate.1.0.0.tar.gz"
+  homepage "http://github.com/zyedidia/Literate"
   sha256 "74087d27e6020d1ddac24eb95f2f65a6bd5707ee1891a8f21d80fe790b068b54"
 
   head "https://github.com/zyedidia/Literate.git"
 
-  depends_on "lua"
-  # Better to use universal ctags
-  depends_on "ctags-exuberant" => :optional
+  depends_on "dmd"
+  depends_on "dub"
 
   def install
+    system "make"
     bin.install Dir["bin/lit"]
   end
 
@@ -42,7 +41,7 @@ class Literate < Formula
         }
       EOS
 
-      system "lit", "-code", "#{testpath/"test.lit"}"
+      system "lit", "-t", "#{testpath/"test.lit"}"
       assert_match File.read("hello_world.c").strip, File.read("ans.c").strip
   end
 end
